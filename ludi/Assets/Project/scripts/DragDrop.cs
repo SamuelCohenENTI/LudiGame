171d32a064036069cfb30a6c395c69953a87e3e5
MaxAlbero <maximo.albero@enti.cat>
2025-10-15 13:15:34 +0200

    Modified DragDrop script
    I need to finish the functionality of this script

--- ludi/Assets/Project/scripts/DragDrop.cs
+++ ludi/Assets/Project/scripts/DragDrop.cs
@@ -1,24 +1,45 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
     [SerializeField] private Canvas canvas;
     private RectTransform rectTransform;
+    private Button button;
+    private Color newColor;
+
+    private void Start()
+    {
+        newColor = Color.green;
+    }
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        button = GetComponent<Button>();
     }
 
-    public void OnPointerDown(PointerEventData eventData) { }
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("Pointer Down");
+    }
 
-    public void OnBeginDrag(PointerEventData eventData) { }
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("Begin Drag");
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
-    public void OnEndDrag(PointerEventData eventData) { }
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("End Drag");
+    }
+
+    public void OnDrop(PointerEventData eventData) {
+        ColorBlock cb = new ColorBlock();
+        cb.disabledColor = newColor;
+        button.colors = cb;
+    }
 }
