595fffcd1650bd2d4d8fee2299ebaf665d0851c9
MaxAlbero <maximo.albero@enti.cat>
2025-10-16 00:01:18 +0200

    Added a go back button

--- ludi/Assets/Project/scripts/GamesMenuSelection.cs
+++ ludi/Assets/Project/scripts/GamesMenuSelection.cs
@@ -13,4 +13,19 @@ public class GamesMenuSelection : MonoBehaviour
         Application.Quit();
     }
 
+    public void LoadPreviousScene()
+    {
+        // Obtiene el índice de la escena actual
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Verifica que no estemos en la primera escena (índice 0)
+        if (currentSceneIndex > 0)
+        {
+            SceneManager.LoadScene(currentSceneIndex - 1);
+        }
+        else
+        {
+            Debug.LogWarning("No hay una escena anterior para cargar.");
+        }
+    }
 }
